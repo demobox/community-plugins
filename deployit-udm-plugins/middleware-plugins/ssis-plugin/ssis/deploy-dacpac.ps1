ee8c04dd34663d64d5821d041ebec2231bfa650f
coert <cvdnthillart@gmail.com>
2014-04-11 10:18:44 +0200

    Added .dacpac deployment support

---
+++ deployit-udm-plugins/middleware-plugins/ssis-plugin/ssis/deploy-dacpac.ps1
@@ -0,0 +1,55 @@
+#first check Powershell compatibility
+if($PSVersionTable.PSVersion.major -lt 3) {
+    Write-Error "This script requires Powershell version 3 or higher to run"
+    Exit
+}
+
+#Variables
+$fullDacPacPath = $deployed.file
+$SqlServer      = $deployed.serverName
+$TargetDatabase = $deployed.targetDatabase
+
+<#
+$assemblylist = 
+"Microsoft.SqlServer.Dac",
+"Microsoft.SqlServer.Dac.DacServices",
+"Microsoft.SqlServer.Management.Dac",
+"Microsoft.SqlServer.Management.DacEnum"
+
+foreach ($asm in $assemblylist)
+{
+    $asm = [System.Reflection.Assembly]::LoadWithPartialName($asm)
+}
+#>
+
+add-type -path $deployed.dacDllPath
+ 
+Write-Host "Deploying the DB with the following settings" 
+Write-Host "SQL Server:   $SqlServer" 
+Write-Host "Dacpac: $fullDacPacPath" 
+Write-Host "Target Database: $TargetDatabase"
+
+$connectionString = "server=$SqlServer"
+if($deployed.userName -and $deployed.password){
+    Write-Host "Using provided credentials for user $($deployed.userName)."
+    $connectionString = "server=$SqlServer;User ID=$($deployed.userName);Password=$($deployed.password)"
+}
+
+$d = new-object Microsoft.SqlServer.Dac.DacServices ($connectionString)
+
+# register events, if you want 'em 
+register-objectevent -in $d -eventname Message -source "msg" -action { out-host -in $Event.SourceArgs[1].Message.Message } | Out-Null
+
+# Load dacpac from file & deploy to database named pubsnew
+$dp = [Microsoft.SqlServer.Dac.DacPackage]::Load($fullDacPacPath)
+$DeployOptions = new-object Microsoft.SqlServer.Dac.DacDeployOptions
+$DeployOptions.IncludeCompositeObjects   = $deployed.includeCompositeObjects
+$DeployOptions.IgnoreFileSize            = $deployed.ignoreFileSize
+$DeployOptions.IgnoreFilegroupPlacement  = $deployed.ignoreFilegroupPlacement
+$DeployOptions.IgnoreFileAndLogFilePath  = $deployed.ignoreFileAndLogFilePath
+$DeployOptions.AllowIncompatiblePlatform = $deployed.allowIncompatiblePlatform
+
+$d.Deploy($dp, $TargetDatabase,$true,$DeployOptions) 
+
+# clean up event 
+unregister-event -source "msg" 
